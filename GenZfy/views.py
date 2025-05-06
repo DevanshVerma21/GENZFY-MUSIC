9d462382c9340c24f3c638b42c80ed84242eb1fd
As262 <avinavsaini336@gmail.com>
2025-05-07 00:49:53 +0530

    added sign up function

--- GenZfy/views.py
+++ GenZfy/views.py
@@ -4,3 +4,9 @@ def welcome(request):
     return render(request, 'GenZfy/welcome.html')
 def playlist(request):
     return render(request, 'GenZfy/playlist.html')
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+    return render(request, 'GenZfy/signup.html')
