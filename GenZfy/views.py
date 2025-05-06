6a6cada81dd2087c08fce9c532514afc0745edab
As262 <avinavsaini336@gmail.com>
2025-05-07 00:51:01 +0530

    updated sign up function

--- GenZfy/views.py
+++ GenZfy/views.py
@@ -9,4 +9,18 @@ def signup_view(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+
+        if User.objects.filter(username__iexact=username).exists():
+            messages.error(request, 'Username already exists')
+            return redirect('signup')
+        if User.objects.filter(email__iexact=email).exists():
+            messages.error(request, 'Email already exists')
+            return redirect('signup')
+
+        User.objects.create(username=username, email=email, password=password)
+        messages.success(request, 'Signup successful! Redirecting to login...')
+        return render(request, 'GenZfy/signup.html', {
+            'signup_success': True,
+            'redirect_url': '/login/'
+        })
     return render(request, 'GenZfy/signup.html')
