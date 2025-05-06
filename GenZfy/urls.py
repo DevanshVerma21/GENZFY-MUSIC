47a7e648555beeaa051938fd5a98c8e6dfb454df
As262 <avinavsaini336@gmail.com>
2025-05-07 01:09:50 +0530

    added contact redirect'

--- GenZfy/urls.py
+++ GenZfy/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('contact/', views.contact, name='contact'),
+    
 ]
